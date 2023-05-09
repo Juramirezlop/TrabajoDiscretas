e7074eb4c3f0eb6b623077e4281d029cabf409a5
diegomel07 <dimellizojr@hotmail.es>
2023-05-09 12:17:24 -0500

    final

--- Proyecto_1.py
+++ Proyecto_1.py
@@ -1,7 +1,8 @@
 print('Ingresa la cadena de caracteres: ')
-cad = input()
+cad = input().lower()
 n = 0
 m = 0
+t = 0
 for i in cad:
     letra = ord(i)
     if letra == 97:
@@ -9,8 +10,10 @@ for i in cad:
     elif letra == 98:
         m = m+1
     else:
-        continue
-if 2*n == m:
+        t = t+1
+if t >= 1:
+    print('No pertenece al conjunto')
+elif 2*n == m:
     print('Pertenece al conjunto')
 else:
     print('No pertenece al conjunto')
